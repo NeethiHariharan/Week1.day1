49c8e4baf580811f5fac1f1d50a8127b84c3e917
NeethiHariharan <98328949+NeethiHariharan@users.noreply.github.com>
2022-01-26 20:01:37 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,24 @@
+package looppractice;
+
+
+
+public class Student {
+public String Name = "Lakshmi";
+public int  rollno = 123456;
+public void  college()
+{
+	String studentName = "Srihari";
+	System.out.println(Name);
+	
+	
+	}
+public static void main(String[] args) {
+	Student obj =new Student();
+	obj.college();
+	
+	
+	
+
+}
+
+}
