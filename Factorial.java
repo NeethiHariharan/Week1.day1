c49842dc38df01c90b5c8a3138b6aa2e448cbe0b
NeethiHariharan <98328949+NeethiHariharan@users.noreply.github.com>
2022-01-27 17:27:59 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package looppractice;
+
+public class Factorial {
+	  
+	
+	
+	
+	public static void main(String[] args) {
+		int i,fact=1;
+		int num=8;
+		for(i=1;i<=num;i++){
+		fact=fact*i;
+		}
+		System.out.println("Factorial of "+num+" is: "+fact);    
+	}
+}
+		
+		
